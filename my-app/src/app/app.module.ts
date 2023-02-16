b03dc639bc04d641dd7cd683b063f131672b705b
clarardz <clararomerodiaz98@gmail.com>
2023-02-16 14:21:55 +0100

    ,

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Component2Component } from './components/component2/component2.component';
+import { Directive1Directive } from './directives/directive1.directive';
+import { Pipe2Pipe } from './pipes/pipe2.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component2Component,
+    Directive1Directive,
+    Pipe2Pipe
   ],
   imports: [
     BrowserModule
